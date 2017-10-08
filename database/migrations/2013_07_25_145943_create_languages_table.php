b4c3b4596cb0a8c1b6f3897613652b30fb6f6878
Stijn van Nieulande <stijn.van.nieulande@gmail.com>
2017-10-08 12:53:50 +0200

    Update 2013_07_25_145943_create_languages_table.php

--- database/migrations/2013_07_25_145943_create_languages_table.php
+++ database/migrations/2013_07_25_145943_create_languages_table.php
@@ -12,7 +12,7 @@ class CreateLanguagesTable extends Migration
      */
     public function up()
     {
-        Schema::create('translator_languages', function ($table) {
+        Schema::create('MP_translator_languages', function ($table) {
             $table->increments('id');
             $table->string('locale', 6)->unique();
             $table->string('name', 60)->unique();
