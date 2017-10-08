54dc11df1fe88d78950b28e08ae17b9fdbc916fb
Stijn van Nieulande <stijn.van.nieulande@gmail.com>
2017-10-08 11:52:33 +0200

    Update 2016_06_02_124154_increase_locale_length.php

--- database/migrations/2016_06_02_124154_increase_locale_length.php
+++ database/migrations/2016_06_02_124154_increase_locale_length.php
@@ -12,10 +12,10 @@ class IncreaseLocaleLength extends Migration
      */
     public function up()
     {
-        Schema::table('translator_languages', function ($table) {
+        Schema::table('MP_translator_languages', function ($table) {
             $table->string('locale', 10)->change();
         });
-        Schema::table('translator_translations', function ($table) {
+        Schema::table('MP_translator_translations', function ($table) {
             $table->string('locale', 10)->change();
         });
     }
